05554ab758f6234f081de29b3a6a655170379611
Victor Hugo Aguilar <victorhugoaguilar@aol.com>
2022-11-08 14:08:43 +0100

    create bases composeable

---
+++ src/views/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Counter Vue</h1>
+    <h3> {{ counter }} </h3>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+</template>
+
+<script>
+import useCounter from '@/composables/useCounter';
+
+export default {
+    name: 'counter-view',
+    props: {},
+    emits: [],
+    setup() {
+        const { counter, decrease, increase } = useCounter(1);
+
+        return {
+            counter,
+            decrease,
+            increase
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
